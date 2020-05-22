12b0665278edfd3a9ef2719c27deeee2f6d090e9
Your Name <you@example.com>
2020-05-22 10:30:15 -0400

    Second version of project

--- sys_info_page.sh
+++ sys_info_page.sh
@@ -1,1 +1,11 @@
-echo "first project"
+#!/bin/bash
+# Program to output a system information page
+echo "<html>"
+echo " <head>"
+echo " <title>Page Title</title>"
+echo " </head>"
+echo " <body>"
+echo " Page body."
+echo " </body>"
+echo "</html>"
+
